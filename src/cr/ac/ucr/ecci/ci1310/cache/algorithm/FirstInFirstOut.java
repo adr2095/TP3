af2a284e61ecc5ce919fb17cda04aea1e48b490f
DanielD <danidiaza@gmail.com>
2017-12-10 10:05:21 -0600

    Correcciones menores de generics

--- src/cr/ac/ucr/ecci/ci1310/cache/algorithm/FirstInFirstOut.java
+++ src/cr/ac/ucr/ecci/ci1310/cache/algorithm/FirstInFirstOut.java
@@ -1,27 +1,27 @@
 package cr.ac.ucr.ecci.ci1310.cache.algorithm;
-import cr.ac.ucr.ecci.ci1310.util.MyLinkedList.LinkedNode;
+import cr.ac.ucr.ecci.ci1310.util.MyLinkedList.*;
 import cr.ac.ucr.ecci.ci1310.cache.CacheMemory;
 
 public class FirstInFirstOut <K,V> extends CacheMemory <K,V> {
-    protected LinkedNode Lookup(K key) {
+    protected LinkedNode<CacheEntry> Lookup(K key) {
         return null;
     }
 
-    protected LinkedNode Insert(K key, V value) {
+    protected LinkedNode<CacheEntry> Insert(K key, V value) {
         return null;
     }
 
-    protected LinkedNode Set(LinkedNode n, V value) {
+    protected LinkedNode<CacheEntry> Set(LinkedNode<CacheEntry> n, V value) {
         return null;
     }
 
 
-    protected LinkedNode SelectVictim()
+    protected LinkedNode<CacheEntry> SelectVictim()
     {
         return elementList.RemoveFirst();
     }
 
-    protected void Delete(K key) {
+    protected void Delete(LinkedNode<CacheEntry> node) {
 
     }
 
